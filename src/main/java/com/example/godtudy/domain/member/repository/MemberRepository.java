44ae5af746dbbd383c23ee60dece6be87224c468
YuHayeon <youha0930@gmail.com>
2022-07-26 20:46:51 +0900

    feat: 학생 검색 기능

--- src/main/java/com/example/godtudy/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/godtudy/domain/member/repository/MemberRepository.java
@@ -1,10 +1,13 @@
 package com.example.godtudy.domain.member.repository;
 
 import com.example.godtudy.domain.member.entity.Member;
+import com.example.godtudy.domain.member.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Transactional(readOnly = true)
@@ -22,5 +25,5 @@ public interface MemberRepository extends JpaRepository<Member, Long>, JpaSpecif
 
     Optional<Member> findByNickname(String email);
 
-
+    List<Member> findByUsernameContainsAndRole(@Param("username") String username, Role role);
 }
