b5af9ce8dc16f0a99959dafdb1a16d252c83e1e3
sangwon <swchoi1997@naver.com>
2022-07-27 23:05:17 +0900

    add: repository, dto, service / feat: comment save

---
+++ src/main/java/com/example/godtudy/domain/comment/dto/CommentSaveDto.java
@@ -0,0 +1,22 @@
+package com.example.godtudy.domain.comment.dto;
+
+import com.example.godtudy.domain.comment.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentSaveDto {
+
+    private String content;
+
+    public Comment toEntity() {
+        return Comment.builder().content(content).childComment(new ArrayList<>()).build();
+    }
+}
---
+++ src/main/java/com/example/godtudy/domain/comment/service/CommentService.java
@@ -0,0 +1,18 @@
+package com.example.godtudy.domain.comment.service;
+
+import com.example.godtudy.domain.comment.dto.CommentSaveDto;
+import com.example.godtudy.domain.comment.dto.CommentUpdateDto;
+import com.example.godtudy.domain.member.entity.Member;
+
+public interface CommentService {
+
+    void save(Long postId, Member member, CommentSaveDto commentSaveDto);
+
+    void saveReComment(Long postId, Member member, Long firstCommentId, CommentSaveDto commentSaveDto);
+
+    void update(Long id,  Member member, CommentUpdateDto commentUpdateDto);
+
+    void delete(Long id,  Member member,);
+
+
+}
