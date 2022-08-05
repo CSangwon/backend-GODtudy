44aeeb466e0f94477f16cb0761baf1cf6c0ef041
YuHayeon <youha0930@gmail.com>
2022-08-05 16:38:45 +0900

    feat: 전체 프로필 조회 기능 추가 (username, Role 검색 가능)

--- src/main/java/com/example/godtudy/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/godtudy/domain/member/repository/MemberRepository.java
@@ -2,12 +2,13 @@ package com.example.godtudy.domain.member.repository;
 
 import com.example.godtudy.domain.member.entity.Member;
 import com.example.godtudy.domain.member.entity.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 @Transactional(readOnly = true)
@@ -27,5 +28,6 @@ public interface MemberRepository extends JpaRepository<Member, Long>, JpaSpecif
 
     Optional<Member> findByNickname(String email);
 
-    List<Member> findByUsernameContainsAndRole(@Param("username") String username, @Param("role") Role role);
+    Page<Member> findByUsernameContainsAndRole(@Param("username") String username, @Param("role") Role role, Pageable pageable);
+
 }
