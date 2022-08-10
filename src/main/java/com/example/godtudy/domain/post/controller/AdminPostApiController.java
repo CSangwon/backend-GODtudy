45b81bd51f599ecdc590501db4e1021dd32d1750
sangwon <swchoi1997@naver.com>
2022-08-10 21:31:21 +0900

    feat : 페이징 처리 + 검색기능 controller 추가

--- src/main/java/com/example/godtudy/domain/post/controller/AdminPostApiController.java
+++ src/main/java/com/example/godtudy/domain/post/controller/AdminPostApiController.java
@@ -3,13 +3,18 @@ package com.example.godtudy.domain.post.controller;
 import com.example.godtudy.domain.member.entity.CurrentMember;
 import com.example.godtudy.domain.member.entity.Member;
 import com.example.godtudy.domain.post.dto.request.PostSaveRequestDto;
+import com.example.godtudy.domain.post.dto.request.PostSearchCondition;
 import com.example.godtudy.domain.post.dto.request.PostUpdateRequestDto;
 import com.example.godtudy.domain.post.service.AdminPostService;
 import com.example.godtudy.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,32 +34,39 @@ public class AdminPostApiController {
 
 
     @PostMapping("{post}/new")
-    public ResponseEntity<?> createNoticeEvent(@PathVariable String post ,@CurrentMember Member member,
+    public ResponseEntity<?> createAdminPost(@PathVariable String post, @CurrentMember Member member,
                                                @RequestBody PostSaveRequestDto postSaveRequestDto,
                                                @RequestPart List<MultipartFile> files) throws IOException {
         if (files.isEmpty()) {
             return postService.createPost(member, post, postSaveRequestDto);
         }
-        return postService.createPost(member,files, post, postSaveRequestDto);
+        return postService.createPost(member, files, post, postSaveRequestDto);
     }
 
-    @PostMapping("/{post}/{postId}")
-    public ResponseEntity<?> updateNoticeEvent(@PathVariable String post, @PathVariable Long postId, @CurrentMember Member member,
+    @PostMapping("/{post}/{postId}") // 게시글 수정
+    public ResponseEntity<?> updateAdminPost(@PathVariable String post, @PathVariable Long postId, @CurrentMember Member member,
                                                @RequestBody PostUpdateRequestDto postUpdateRequestDto,
                                                @RequestPart List<MultipartFile> files) throws IOException {
         if (files.isEmpty()) {
-            return postService.updatePost(member, post, postId , postUpdateRequestDto);
+            return postService.updatePost(member, post, postId, postUpdateRequestDto);
         }
-        return postService.updatePost(member, post,files, postId , postUpdateRequestDto);
+        return postService.updatePost(member, post, files, postId, postUpdateRequestDto);
     }
 
-    @DeleteMapping("/{post}/{postId}")
-    public ResponseEntity<?> deleteNoticeEvent(@PathVariable String post, @PathVariable Long postId, @CurrentMember Member member) {
-        return postService.deletePost(member, postId);
+    @DeleteMapping("/{post}/{postId}") // 게시글 삭제
+    public ResponseEntity<?> deleteAdminPost(@PathVariable String post, @PathVariable Long postId, @CurrentMember Member member) {
+        return postService.deletePost(member, post, postId);
     }
 
-    @GetMapping("{post}/{postId}")
-    public ResponseEntity<?> getPostInfo(@PathVariable String post, @PathVariable Long postId) {
+    @GetMapping("{post}/{postId}") // 게시글 조회
+    public ResponseEntity<?> getAdminPostInfo(@PathVariable String post, @PathVariable Long postId) {
         return new ResponseEntity<>(postService.getPostInfo(postId), HttpStatus.OK);
+    }
+
+    @GetMapping("{post}")
+    public ResponseEntity<?> getAdminPostList(@PathVariable String post,
+                                              @RequestBody PostSearchCondition postSearchCondition,
+                                              @PageableDefault(size = 12, sort = "createdDate", direction = Sort.Direction.DESC) Pageable pageable) {
+        return new ResponseEntity<>(postService.getPostList(pageable, postSearchCondition), HttpStatus.OK);
     }
 }
