be49b57e057eb7364aa90cfac0c16e8177cbe7d0
YuHayeon <youha0930@gmail.com>
2022-08-15 01:45:30 +0900

    add: TodoApiController 추가

---
+++ src/main/java/com/example/godtudy/domain/todo/controller/TodoApiController.java
@@ -0,0 +1,60 @@
+package com.example.godtudy.domain.todo.controller;
+
+import com.example.godtudy.domain.todo.dto.request.CreateTodoRequestDto;
+import com.example.godtudy.domain.todo.dto.request.UpdateTodoRequestDto;
+import com.example.godtudy.domain.todo.dto.response.TodoDto;
+import com.example.godtudy.domain.todo.service.TodoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/study")
+@RequiredArgsConstructor
+@RestController
+public class TodoApiController {
+
+    private final TodoService todoService;
+
+    @GetMapping("/{studyUrl}/todo/{id}")
+    public ResponseEntity<TodoDto> getTodo(@PathVariable("id") Long todoId) {
+        TodoDto todo = todoService.getTodo(todoId);
+        return new ResponseEntity<>(todo, HttpStatus.OK);
+    }
+
+    @GetMapping("/{studyUrl}/todo")
+    public ResponseEntity<Page<TodoDto>> getTodos(
+            @PathVariable("studyUrl") String studyUrl,
+            @PageableDefault(size = 12, sort = "endDate", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<TodoDto> todoDtoPage = todoService.getTodos(studyUrl, pageable);
+        return new ResponseEntity<>(todoDtoPage, HttpStatus.OK);
+    }
+
+    @PostMapping("/{studyUrl}/todo")
+    public ResponseEntity<TodoDto> createTodo(
+            @PathVariable("studyUrl") String studyUrl,
+            @RequestBody CreateTodoRequestDto createTodoRequestDto) {
+        TodoDto todoDto = todoService.createTodo(studyUrl, createTodoRequestDto);
+        return new ResponseEntity<>(todoDto, HttpStatus.OK);
+    }
+
+    @PutMapping("/{studyUrl}/todo/{id}")
+    public ResponseEntity<Long> updateTodo(
+            @PathVariable("id") Long todoId,
+            @RequestBody UpdateTodoRequestDto updateTodoRequestDto) {
+        Long updateTodoId = todoService.updateTodo(todoId, updateTodoRequestDto);
+        return new ResponseEntity<>(updateTodoId, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{studyUrl}/todo/{id}")
+    public ResponseEntity<?> deleteTodo(
+            @PathVariable("studyUrl") String studyUrl,
+            @PathVariable("id") Long todoId) {
+        todoService.deleteTodo(studyUrl, todoId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
